07c89870f2f41689dc27017593cab83fc46d64eb
akdsksdyd <kakalote4@gmail.com>
2023-04-27 16:10:46 +0900

    first commit

---
+++ codinghub/src/main/java/com/codinghub/CodinghubApplication.java
@@ -0,0 +1,13 @@
+package com.codinghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinghubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinghubApplication.class, args);
+	}
+
+}
